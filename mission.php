9002766bc24255c7c8402448f9df78e1e7629698
ddvdv <vdv.david@gmail.com>
2017-09-15 11:31:00 +0200

    Add mission contenu

--- mission.php
+++ mission.php
@@ -3,16 +3,74 @@
 	Mission
 </h2>
 <p>
-	<span id="docs-internal-guid-27cf4c8a-84b1-caf7-a0ab-71f9d9fc2188"></span>
+	We believe in sustainable growth in an European economy where no one should be left behind.
+	We act towards this by investing in the best solutions for job creation and access to work,
+	from cradle to career.
+</p>
+
+<p>
+	The 3 pillars of Degroof Petercam Foundation:
 </p>
 
 <div>
-	<span id="docs-internal-guid-27cf4c8b-84b3-c74d-a150-6a479c1196c5"></span>
-	
-	
+	<!-- Schéma + liens -->
 </div>
 
-<p>
-	<a class="btn" href="#">View details »</a>
-</p>
+<!-- Bloc EDUCATION -->
+<div class="education">
+	<h2>Education</h2>
+	<p>
+		The Degroof Petercam Foundation supports education by helping to reduce the number of school dropouts.
+		The main focus is on children from low-come families and who are socially disadvantaged.
+		That’s why we’re contributing to quality, equitable education by supporting students with:
+		<ul>
+			<li>Coaching and guidance</li>
+			<li>Encouragement</li>
+			<li>Information about new skills that they'll need in the future</li>
+			<li>reassessment of technical and vocational education</li>
+		</ul>
+	</p>
+	<p>
+		And teachers with:
+		<ul>
+			<li>Coaching and guidance</li>
+			<li>Encouragement</li>
+			<li>Revaluation of the teaching profession</li>
+			<li>Informational and educational workshops</li>
+		</ul>
+	</p>
+</div>
+<!-- End EDUCATION -->
+
+<!-- Bloc Entrepreneurship -->
+<div class="entrepreneurship">
+	<h2>Education</h2>
+	<p>
+		Entrepreneurship was selected as the second priority pillar because the Foundation is convinced
+		that the creation of enterprises is an effective and measurable solution to the economic challenges
+		facing future generations.
+	</p>
+	<p>
+		The Foundation aims to encourage the creation of businesses and job creation by:
+		<ul>
+			<li>Highlighting entrepreneurship and the role of the entrepreneur</li>
+			<li>Promoting entrepreneurship among young people</li>
+			<li>Supporting new initiatives to boost entrepreneurship</li>
+		</ul>
+	</p>
+</div>
+<!-- End Entrepreneurship -->
+
+<!-- Bloc Job Accessibility -->
+<div class="job-accessibility">
+	<h2>Job Accessibility</h2>
+		<ul>
+			<li>Give access to jobs for more underpriviledged</li>
+			<li>Being a catalyst, activator</li>
+			<li>Match job supply and demand</li>
+			<li>...</li>
+		</ul>
+</div>
+<!-- End Job Accessibility -->
+
 </section>
