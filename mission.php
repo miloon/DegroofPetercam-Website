e1cb3932f210f9df80f8140f67eed6b9e908433f
ddvdv <vdv.david@gmail.com>
2017-09-15 13:54:06 +0200

    MAJ mission + ADD style.css

--- mission.php
+++ mission.php
@@ -1,76 +1,112 @@
 <section id="mission">
-<h2>
-	Mission
-</h2>
-<p>
-	We believe in sustainable growth in an European economy where no one should be left behind.
-	We act towards this by investing in the best solutions for job creation and access to work,
-	from cradle to career.
-</p>
+	<h2 class="section-title">Mission</h2>
+	<div class="row">
+		<div class="col-md-offset-1 col-md-10">
+			<p>
+				We believe in <strong>sustainable growth</strong> in an European economy where no one should be left behind.
+				We act towards this by investing in the best solutions for <strong>job creation</strong> and <strong>access to work,
+				from cradle to career.</strong>
+			</p>
 
-<p>
-	The 3 pillars of Degroof Petercam Foundation:
-</p>
+			<p>
+				The 3 pillars of Degroof Petercam Foundation:
+			</p>
+		</div>
+	</div>
 
-<div>
-	<!-- Schéma + liens -->
+	<div class="container pillars-container">
+		<!-- Schéma + liens -->
+		<div class="first-little-box">
+			<!-- First panel -->
+			Jobs for sustainable Growth
+		</div>
+
+		<div class="row">
+			<div class="col-xs-6 col-sm-6 col-md-12">
+				<a href="#"><div class="panel-education little-box">
+					<!-- Education panel -->
+					Education (youth)
+				</div></a>
+				<a href="#"><div class="entrepreneurship little-box">
+					<!-- Entrepreneurship pan -->
+					Entrepreneurship
+				</div></a>
+				<a href="#"><div class="job-accessibility-panel little-box">
+					<!-- Entrepreneurship pan -->
+					Job Accessibility
+				</div></a>
+			</div>
+		</div>
+
+	</div>
+
+	<!-- Bloc EDUCATION -->
+	<div class="education">
+		<h2>Education</h2>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10 back-missions">
+				<p>
+					The Degroof Petercam Foundation supports education by helping to reduce the number of school dropouts.
+					The main focus is on children from low-come families and who are socially disadvantaged.
+					That’s why we’re contributing to quality, equitable education by supporting students with:
+					<ul>
+						<li>Coaching and guidance</li>
+						<li>Encouragement</li>
+						<li>Information about new skills that they'll need in the future</li>
+						<li>Reassessment of technical and vocational education</li>
+					</ul>
+				</p>
+				<p>
+					And teachers with:
+					<ul>
+						<li>Coaching and guidance</li>
+						<li>Encouragement</li>
+						<li>Revaluation of the teaching profession</li>
+						<li>Informational and educational workshops</li>
+					</ul>
+				</p>
+			</div>
+		</div>
+	</div>
+	<!-- End EDUCATION -->
+
+	<!-- Bloc Entrepreneurship -->
+	<div class="entrepreneurship">
+		<h2>Entrepreneurship</h2>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10 back-missions">
+				<p>
+					Entrepreneurship was selected as the second priority pillar because the Foundation is convinced
+					that the creation of enterprises is an effective and measurable solution to the economic challenges
+					facing future generations.
+				</p>
+				<p>
+					The Foundation aims to encourage the creation of businesses and job creation by:
+					<ul>
+						<li>Highlighting entrepreneurship and the role of the entrepreneur</li>
+						<li>Promoting entrepreneurship among young people</li>
+						<li>Supporting new initiatives to boost entrepreneurship</li>
+					</ul>
+				</p>
+			</div>
+		</div>
+	</div>
+	<!-- End Entrepreneurship -->
+
+	<!-- Bloc Job Accessibility -->
+	<div class="job-accessibility">
+		<h2>Job Accessibility</h2>
+		<div class="row">
+			<div class="col-md-offset-1 col-md-10 back-missions">
+				<ul>
+					<li>Give access to jobs for more underpriviledged</li>
+					<li>Being a catalyst, activator</li>
+					<li>Match job supply and demand</li>
+					<li>...</li>
+				</ul>
+	</div>
 </div>
-
-<!-- Bloc EDUCATION -->
-<div class="education">
-	<h2>Education</h2>
-	<p>
-		The Degroof Petercam Foundation supports education by helping to reduce the number of school dropouts.
-		The main focus is on children from low-come families and who are socially disadvantaged.
-		That’s why we’re contributing to quality, equitable education by supporting students with:
-		<ul>
-			<li>Coaching and guidance</li>
-			<li>Encouragement</li>
-			<li>Information about new skills that they'll need in the future</li>
-			<li>reassessment of technical and vocational education</li>
-		</ul>
-	</p>
-	<p>
-		And teachers with:
-		<ul>
-			<li>Coaching and guidance</li>
-			<li>Encouragement</li>
-			<li>Revaluation of the teaching profession</li>
-			<li>Informational and educational workshops</li>
-		</ul>
-	</p>
 </div>
-<!-- End EDUCATION -->
-
-<!-- Bloc Entrepreneurship -->
-<div class="entrepreneurship">
-	<h2>Education</h2>
-	<p>
-		Entrepreneurship was selected as the second priority pillar because the Foundation is convinced
-		that the creation of enterprises is an effective and measurable solution to the economic challenges
-		facing future generations.
-	</p>
-	<p>
-		The Foundation aims to encourage the creation of businesses and job creation by:
-		<ul>
-			<li>Highlighting entrepreneurship and the role of the entrepreneur</li>
-			<li>Promoting entrepreneurship among young people</li>
-			<li>Supporting new initiatives to boost entrepreneurship</li>
-		</ul>
-	</p>
-</div>
-<!-- End Entrepreneurship -->
-
-<!-- Bloc Job Accessibility -->
-<div class="job-accessibility">
-	<h2>Job Accessibility</h2>
-		<ul>
-			<li>Give access to jobs for more underpriviledged</li>
-			<li>Being a catalyst, activator</li>
-			<li>Match job supply and demand</li>
-			<li>...</li>
-		</ul>
-</div>
-<!-- End Job Accessibility -->
+	<!-- End Job Accessibility -->
 
 </section>
