1004962de779c255fe0589684df1c309792912d6
Mooch <t.tonneau@gmail.com>
2017-09-15 10:51:38 +0200

    mise en place de la structure du projet

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+require_once 'header.php';
+require_once 'home.php';
+require_once 'mission.php';
+require_once 'challenge.php';
+require_once 'sponsorship';
+require_once 'contact.php';
+require_once 'projects.php';
+require_once 'footer.php';
+
+?>
