532566801e835aaf50153b3a70bdbdf94f710aad
Mooch <t.tonneau@gmail.com>
2017-09-15 11:03:06 +0200

    remplissage contenu from layoutit

---
+++ mission.php
@@ -0,0 +1,18 @@
+<section id="mission">
+<h2>
+	Mission
+</h2>
+<p>
+	<span id="docs-internal-guid-27cf4c8a-84b1-caf7-a0ab-71f9d9fc2188"></span>
+</p>
+
+<div>
+	<span id="docs-internal-guid-27cf4c8b-84b3-c74d-a150-6a479c1196c5"></span>
+	
+	
+</div>
+
+<p>
+	<a class="btn" href="#">View details »</a>
+</p>
+</section>
