64db6216faac2b0dea3bf73e60ca5d15e07d6ed0
Mooch <t.tonneau@gmail.com>
2017-09-15 15:58:56 +0200

    final

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 
 					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
 						 <span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span>
-					</button> <a class="navbar-brand" href="#home"><img id="logo-header" src="logo.png" alt="logo de Degroof Petercam Foundation"></a>
+					</button> <a href="#home"><img id="logo-header" src="logo.png" alt="logo de Degroof Petercam Foundation"></a>
 
 
 				</div>
